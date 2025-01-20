60af82afa1306bf8e50a07136688291309f5ba97
mohamedjamaal <mohamed_gamal19@hotmail.com>
2025-01-20 15:10:58 +0200

    finish splash screen

--- ecommerce/lib/core/helper_functions/on_generate_routes.dart
+++ ecommerce/lib/core/helper_functions/on_generate_routes.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:ecommerce/features/splash/presentation/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,8 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashScreen.routeName:
       return MaterialPageRoute(builder: (_) => const SplashScreen());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (_) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (_) => const SplashScreen());
   }
